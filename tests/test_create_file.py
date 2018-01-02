b22fded5d63236033df1f61ba0c31e93abc0bf83
Ben <ben_schuenemann@yahoo.de>
2018-01-02 11:54:08 +0100

    test uses tmpdir

--- tests/test_create_file.py
+++ tests/test_create_file.py
@@ -1,14 +1,13 @@
 from os.path import isfile
 
 
-def create_file(name):
-    with open(name, 'w') as f:
-        f.close()
+def create_file(file):
+    file.open(mode = 'w')
 
 
-def test_create_file():
-    source_file = "bla.py"
+def test_create_file(tmpdir):
+    source_file = tmpdir.join("bla.py")
     create_file(source_file)
-    test_file = "test_bla.py"
+    test_file  = tmpdir.join("test_bla.py")
     create_file(test_file)
-    assert isfile(test_file)
+    assert test_file.check() # check that file exists
