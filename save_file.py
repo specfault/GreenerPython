0b12715880065cdf62880e677f4e48259409d32b
Ben <ben_schuenemann@yahoo.de>
2018-01-18 16:58:52 +0100

    remove a bit of copy&paste by adding type MissingVariable

--- save_file.py
+++ save_file.py
@@ -4,11 +4,13 @@
 import sys
 import subprocess
 from py import path
+from collections import namedtuple
 
 
 MISSING_IMPORT = object()
-MISSING_VARIABLE_X = object()
-MISSING_VARIABLE_Y = object()
+
+
+MissingVariable = namedtuple('MissingVariable', ['name'])
 
 
 def problem(file):
@@ -17,9 +19,9 @@ def problem(file):
         return None
     except subprocess.CalledProcessError as e:
         if "'module' object has no attribute 'x'" in e.output:
-            return MISSING_VARIABLE_X
+            return MissingVariable('x')
         if "'module' object has no attribute 'y'" in e.output:
-            return MISSING_VARIABLE_Y
+            return MissingVariable('y')
         return MISSING_IMPORT
 
 
@@ -42,18 +44,10 @@ if __name__ == '__main__':
             # this didn't fix the problem
             # -> restore the previous content
             file.write(content)
-    if issue is MISSING_VARIABLE_X:
+    if type(issue) == MissingVariable:
         source_file = path.local(file.dirname).join('..').join(filename + '.py')
         content = source_file .read()
-        source_file.write('x = None' + '\n\n\n' + content)
-        if not passes(file):
-            # this didn't fix the problem
-            # -> restore the previous content
-            source_file.write(content)
-    if issue is MISSING_VARIABLE_Y:
-        source_file = path.local(file.dirname).join('..').join(filename + '.py')
-        content = source_file .read()
-        source_file.write('y = None' + '\n\n\n' + content)
+        source_file.write(issue.name + ' = None' + '\n\n\n' + content)
         if not passes(file):
             # this didn't fix the problem
             # -> restore the previous content
