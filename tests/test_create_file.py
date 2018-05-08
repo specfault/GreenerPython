37d1f1514f26a1cbf53fa626a7157b9602def2ca
Ben <ben_schuenemann@yahoo.de>
2018-05-08 21:59:32 +0200

    remove tmpdir from test_create_file

--- tests/test_create_file.py
+++ tests/test_create_file.py
@@ -1,45 +1,58 @@
 from . import vim
+from py import path
+from tempfile import TemporaryDirectory
 
 
-def test_create_test_file_in_source_directory(tmpdir):
+class TemporaryPath():
+    def __init__(self):
+        self.resource = TemporaryDirectory()
+        self.path = path.local(self.resource.name)
+
+
+def test_create_test_file_in_source_directory():
     """there's no 'tests' directory
     so the test file should be created next to the corresponding source file"""
-    source_file = tmpdir.join("bla.py")
+    tmp = TemporaryPath()
+    source_file = tmp.path.join("bla.py")
     vim.save(source_file)
-    test_file = tmpdir.join("test_bla.py")
+    test_file = tmp.path.join("test_bla.py")
     assert test_file.check()  # check that file exists
 
 
-def test_create_test_file_inside_test_directory(tmpdir):
+def test_create_test_file_inside_test_directory():
     """test directory in the same folder as the source file"""
-    test_dir = tmpdir.mkdir('tests')
-    source_file = tmpdir.join("bla.py")
+    tmp = TemporaryPath()
+    test_dir = tmp.path.mkdir('tests')
+    source_file = tmp.path.join("bla.py")
     vim.save(source_file)
     test_file = test_dir.join("test_bla.py")
     assert test_file.check()  # check that file exists
 
 
-def test_create_test_file_inside_test_directory_further_up(tmpdir):
+def test_create_test_file_inside_test_directory_further_up():
     """directory structure of source file should be duplicated inside tests"""
-    source_dir = tmpdir.mkdir('blubb')
-    test_dir = tmpdir.mkdir('tests')
+    tmp = TemporaryPath()
+    source_dir = tmp.path.mkdir('blubb')
+    test_dir = tmp.path.mkdir('tests')
     source_file = source_dir.join("bla.py")
     vim.save(source_file)
     test_file = test_dir.join('blubb').join("test_bla.py")
     assert test_file.check()  # check that file exists
 
 
-def test_create_test_file_directly(tmpdir):
+def test_create_test_file_directly():
     """creating a test file should not create a test for the test"""
-    test_dir = tmpdir.mkdir('tests')
-    source_file = tmpdir.join("test_bla.py")
+    tmp = TemporaryPath()
+    test_dir = tmp.path.mkdir('tests')
+    source_file = tmp.path.join("test_bla.py")
     vim.save(source_file)
     assert not test_dir.listdir()  # no test for the test
 
 
-def test_create_magic_file(tmpdir):
+def test_create_magic_file():
     """don't create tests for magic files"""
-    test_dir = tmpdir.mkdir('tests')
-    source_file = tmpdir.join("__init__.py")
+    tmp = TemporaryPath()
+    test_dir = tmp.path.mkdir('tests')
+    source_file = tmp.path.join("__init__.py")
     vim.save(source_file)
     assert not test_dir.listdir()  # no test for the test
