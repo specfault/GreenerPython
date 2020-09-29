57a195df50a9adc9ef895af56f49ac34cb6f5937
Ben <ben.schuenemann@gmail.com>
2020-09-29 14:57:30 +0200

    use decorator in test_fix_code
    break some long lines to improve readability

--- tests/test_fix_code.py
+++ tests/test_fix_code.py
@@ -5,29 +5,24 @@ from tests.framework import standard_test_spec
 from tests.framework import SavingFixesCombination
 from tests.framework import SavingFixesSUT
 from tests.framework import SavingDoesNotTouchBrokenStuff
-from tests.framework import add_tests
+from tests.framework import fixing_test
 
 
 # both, test and SUT, are broken but fixable
-fixable_combinations = [
-    AbstractFilePair(  # missing import in test, missing variable in SUT
-        'blubb',
-        textwrap.dedent("""\
-            import unittest
-
-
-            class TestSomething(unittest.TestCase):
-                def test_something(self):
-                    bla = blubb.x
-            """))
-    ]
-
-
+@fixing_test
 class TestSavingFixesCombination(SavingFixesCombination):
-    pass
+    tests = [
+        AbstractFilePair(  # missing import in test, missing variable in SUT
+            'blubb',
+            textwrap.dedent("""\
+                import unittest
 
 
-add_tests(TestSavingFixesCombination, fixable_combinations)
+                class TestSomething(unittest.TestCase):
+                    def test_something(self):
+                        bla = blubb.x
+                """))
+        ]
 
 
 various_argument_lists = [[], ['arg'], ['arg1', 'arg2']]
@@ -43,112 +38,114 @@ def missing_function_in_source(argument_list):
 
 
 # SUT is broken but fixable
-fixable_SUTs = [
-    standard_test_spec(  # call missing function with literal argument
-        """
-        arg = 1
-        bla = blubb.some_function(arg, 42)
-        """),
-    standard_test_spec(  # call missing function with two literal arguments
-        """
-        bla = blubb.some_function(41, 42)
-        """),
-    standard_test_spec(  # missing function with default argument
-        """
-        arg = 1
-        bla = blubb.some_function(a=42)
-        bla = blubb.some_function(arg)
-        """),
-    standard_test_spec(  # missing function with normal and default argument
-        """
-        arg = 1
-        bla = blubb.some_function(arg, a=42)
-        bla = blubb.some_function(arg)
-        """),
-    standard_test_spec(  # argument names might clash with generated names
-        """
-        arg0 = 1
-        arg1 = 1
-        arg4 = 1
-        bla = blubb.some_function(arg4, arg0, 42, arg1)
-        """),
-    # create object of missing class with unary constructor
-    standard_test_spec(
-        """
-        a = blubb.Something(17)
-        """),
-    standard_test_spec(  # create object of missing class with attribute
-        """
-        a = blubb.Something()
-        b = a.attribute
-        """),
-    standard_test_spec(  # create object of missing class with nullary method
-        """
-        a = blubb.Something()
-        b = a.fun()
-        """),
-    standard_test_spec(  # create object of missing class with unary method
-        """
-        a = blubb.Something(42)
-        """),
-    ] + [missing_function_in_source(args) for args in various_argument_lists]
-
-
+@fixing_test
 class TestSavingFixesSUT(SavingFixesSUT):
-    pass
-
-
-add_tests(TestSavingFixesSUT, fixable_SUTs)
+    tests = [
+        # call missing function with literal argument
+        standard_test_spec(
+            """
+            arg = 1
+            bla = blubb.some_function(arg, 42)
+            """),
+        # call missing function with two literal arguments
+        standard_test_spec(
+            """
+            bla = blubb.some_function(41, 42)
+            """),
+        # missing function with default argument
+        standard_test_spec(
+            """
+            arg = 1
+            bla = blubb.some_function(a=42)
+            bla = blubb.some_function(arg)
+            """),
+        # missing function with normal and default argument
+        standard_test_spec(
+            """
+            arg = 1
+            bla = blubb.some_function(arg, a=42)
+            bla = blubb.some_function(arg)
+            """),
+        # argument names might clash with generated names
+        standard_test_spec(
+            """
+            arg0 = 1
+            arg1 = 1
+            arg4 = 1
+            bla = blubb.some_function(arg4, arg0, 42, arg1)
+            """),
+        # create object of missing class with unary constructor
+        standard_test_spec(
+            """
+            a = blubb.Something(17)
+            """),
+        # create object of missing class with attribute
+        standard_test_spec(
+            """
+            a = blubb.Something()
+            b = a.attribute
+            """),
+        # create object of missing class with nullary method
+        standard_test_spec(
+            """
+            a = blubb.Something()
+            b = a.fun()
+            """),
+        # create object of missing class with unary method
+        standard_test_spec(
+            """
+            a = blubb.Something(42)
+            """),
+        ]\
+        + [missing_function_in_source(args) for args in various_argument_lists]
 
 
 # SUT and test are broken beyond repair
-broken_pairs = [
-    standard_test_spec(  # different number of function arguments
-        """
-        bla = blubb.random_function(42)
-        aaa = blubb.random_function(42, 37)
-        """,
-        """
-        def random_function():
-            pass
-        """),
-    standard_test_spec(  # using non-existent lib
-        'bla = lalelu.x'),
-    # using nonexistent lib variable
-    AbstractFilePair(
-        'blubb',
-        'import collections\n'
-        + in_test_function('Point = collections.random_typo')),
-    # using lib variable as function
-    AbstractFilePair(
-        'blubb',
-        'import math\n' + in_test_function('Point = math.pi()')),
-    # using lib variable as function
-    # SUT having same variable name makes the problem harder
-    AbstractFilePair(
-        'blubb',
-        'import math\n' + in_test_function('Point = math.pi()'),
-        'pi = None\n'),
-    # cryptic and broken import
-    # (doesn't yet deal with wildcards)
-    AbstractFilePair(
-        'blubb',
-        'from lalelu import *\n' + in_test_function('self.assertTrue(True)')),
-    # adding strings and numbers is broken beyond repair
-    # this should test return code JUST_BROKEN
-    standard_test_spec(
-        'a = 3 + "lol"'),
-    standard_test_spec(  # broken function definition (missing colon)
-        'bla = blubb.random_function(42)',
-        """
-        def random_function()
-            pass
-        """)
-        ]
-
-
+@fixing_test
 class TestSavingDoesNotTouchBrokenStuff(SavingDoesNotTouchBrokenStuff):
-    pass
-
-
-add_tests(TestSavingDoesNotTouchBrokenStuff, broken_pairs)
+    tests = [
+        standard_test_spec(  # different number of function arguments
+            """
+            bla = blubb.random_function(42)
+            aaa = blubb.random_function(42, 37)
+            """,
+            """
+            def random_function():
+                pass
+            """),
+        standard_test_spec(  # using non-existent lib
+            'bla = lalelu.x'),
+        # using nonexistent lib variable
+        AbstractFilePair(
+            'blubb',
+            'import collections\n'
+            + in_test_function('Point = collections.random_typo')),
+        # using lib variable as function
+        AbstractFilePair(
+            'blubb',
+            'import math\n'
+            + in_test_function('Point = math.pi()')),
+        # using lib variable as function
+        # SUT having same variable name makes the problem harder
+        AbstractFilePair(
+            'blubb',
+            'import math\n'
+            + in_test_function('Point = math.pi()'),
+            'pi = None\n'),
+        # cryptic and broken import
+        # (doesn't yet deal with wildcards)
+        AbstractFilePair(
+            'blubb',
+            'from lalelu import *\n'
+            + in_test_function('self.assertTrue(True)')),
+        # adding strings and numbers is broken beyond repair
+        # this should test return code JUST_BROKEN
+        standard_test_spec(
+            'a = 3 + "lol"'),
+        standard_test_spec(  # broken function definition (missing colon)
+            'bla = blubb.random_function(42)',
+            """
+            def random_function()
+                pass
+            """)
+            ]
