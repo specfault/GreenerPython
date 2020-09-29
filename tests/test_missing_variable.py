2795850d9ba064157bdefac4f601b2f160e39f7c
Ben <ben.schuenemann@gmail.com>
2020-09-29 15:04:25 +0200

    use decorator in test_missing_variable

--- tests/test_missing_variable.py
+++ tests/test_missing_variable.py
@@ -2,7 +2,7 @@ from tests.framework import AbstractFilePair
 from tests.framework import in_test_function
 from tests.framework import standard_test_spec
 from tests.framework import SavingFixesSUT
-from tests.framework import add_tests
+from tests.framework import fixing_test
 
 
 variable_names = ('x', 'y')
@@ -13,17 +13,12 @@ def missing_variable_in_source(variable_name):
     return AbstractFilePair('blubb', test=test_code)
 
 
-missing_variable_specs = [
-    standard_test_spec(  # add several variables to SUT
-        """
-        bla = blubb.x
-        bla = blubb.y
-        bla = blubb.z""")
-    ] + [missing_variable_in_source(name) for name in variable_names]
-
-
+@fixing_test
 class TestSavingFixesMissingVariables(SavingFixesSUT):
-    pass
-
-
-add_tests(TestSavingFixesMissingVariables, missing_variable_specs)
+    tests = [
+        standard_test_spec(  # add several variables to SUT
+            """
+            bla = blubb.x
+            bla = blubb.y
+            bla = blubb.z""")
+        ] + [missing_variable_in_source(name) for name in variable_names]
