09d6aa27f4e6b87efee8b3b6c2074c746bcb596c
Ben <ben.schuenemann@gmail.com>
2020-09-29 15:01:05 +0200

    use decorator in test_missing_function

--- tests/test_missing_function.py
+++ tests/test_missing_function.py
@@ -1,16 +1,11 @@
 from tests.framework import standard_test_spec
 from tests.framework import SavingFixesSUT
-from tests.framework import add_tests
+from tests.framework import fixing_test
 
 
-missing_function_specs = [
-    # create object of missing class with nullary constructor
-    standard_test_spec("a = blubb.Something()"),
-    standard_test_spec("a = blubb.fun()")]
-
-
+@fixing_test
 class TestSavingFixesMissingFunction(SavingFixesSUT):
-    pass
-
-
-add_tests(TestSavingFixesMissingFunction, missing_function_specs)
+    tests = [
+        # create object of missing class with nullary constructor
+        standard_test_spec("a = blubb.Something()"),
+        standard_test_spec("a = blubb.fun()")]
